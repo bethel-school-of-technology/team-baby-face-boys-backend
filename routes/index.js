cf64fdfddb579a35d39ce56b43ef5e1e18c9331d
rvrios22 <rvrios22@gmail.com>
2022-07-21 19:52:19 -0700

    Added route for creating an account

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-
+const { User } = require('../models');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
@@ -8,7 +8,16 @@ router.get('/', function(req, res, next) {
 
 //Route for creating an account
 router.post('/createaccount', (req, res, next) => {
-  
+  User.create({
+    fullName: req.body.fullName,
+    gamerID: req.body.gamerID,
+    email: req.body.email,
+    password: req.body.password,
+    DOB: req.body.DOB
+  }).then(newUser => {
+    res.json(newUser)
+  }).catch(() => {
+    res.status(400);
+  })
 })
-
 module.exports = router;
