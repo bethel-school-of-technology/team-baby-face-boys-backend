a43a899a7779d28b96d66c83be34518dfd84c3df
rvrios22 <rvrios22@gmail.com>
2022-07-26 23:13:09 -0500

    removed alter:true from sequelize.synch on app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-models.sequelize.sync({ alter:true }).then(() => {
+models.sequelize.sync({ }).then(() => {
     console.log('DB synch')
 });
 
