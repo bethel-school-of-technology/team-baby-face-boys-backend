0363c22f164767e9b4ddeb3bd43fd8a075f60e1c
rvrios22 <rvrios22@gmail.com>
2022-07-26 23:29:58 -0500

    added route to GET posts and started route to POST to forum

--- routes/forum.js
+++ routes/forum.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+const { User, Post } = require('../models');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+/* GET all posts. */
+router.get('/', (req, res, next) => {
+    Post.findAll().then(postList => {
+        res.json(postList)
+    })
+})
 
-module.exports = router;
+//create a post
+router.post('/', (req,res,next) => {
+    Post.create({
+        postTitle: req.body.postTitle,
+        postBody: req.body.postBody
+    }).then(newPost => {
+        res.json(newPost)
+    }).catch(() => {
+        res.status(400)
+    })
+})
+
+  module.exports = router;
