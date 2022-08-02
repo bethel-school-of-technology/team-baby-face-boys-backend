9571b4f94aa91619ba707c0b255b7a39be038a8a
rvrios22 <rvrios22@gmail.com>
2022-08-01 19:32:09 -0700

    added basic routes for profile. JWT needed

--- routes/profile.js
+++ routes/profile.js
@@ -1,6 +1,73 @@
 var express = require('express');
 var router = express.Router();
+const { User, Post } = require('../models');
+
+//GET user by id *
+router.get('/:id', (req, res, next) => {
+    const id = parseInt(req.params.id);
+    User.findOne({
+        where: {
+            id: id
+        }
+    }).then(user => {
+        if (user){
+            res.json({
+                gamerID: user.gamerID
+            })
+        } else {
+            res.status(400).send()
+        }
+    }), err => {
+        res.status(500).send(err)
+    }
+})
+
+//POST to post to forum
+router.post('/:id', (req, res, next) => {
+    Post.create({
+        postTitle: req.body.postTitle,
+        postBody: req.body.postBody
+    }).then(newPost => {
+        res.json(newPost)
+    }).catch(() => {
+        res.status(400)
+    })
+})
+
+//GET for latest post //need to add JWT for this to work
+// router.get('/:id', (req, res, next) => {
+//     const latestPost = parseInt(req.params.id);
+
+//     Post.findOne({
+//         where: {
+//             id: latestPost
+//         }
+//     }).then(latestPost => {
+//         res.json(latestPost)
+//     }).catch(() => {
+//         res.status(400)
+//     })
+// })
 
 
+//PUT for latest post //need to add JWT
+// router.put('/:id', (req, res, next) => {
+//     const id = parseInt(req.params.id);
+//     if(!id || id < 0){
+//         res.status(400).send()
+//     }
+//     Post.update({
+//         postTitle: req.body.postTitle,
+//         postBody: req.body.postBody
+//     }, {
+//         where: {
+//             id: id
+//         }
+//     }).then(() => {
+//         res.status(204).send()
+//     }).catch(() => {
+//         res.status(400).send()
+//     })
+// })
 
 module.exports = router;
